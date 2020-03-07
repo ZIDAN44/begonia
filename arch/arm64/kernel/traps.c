42f74559b94e2e56c28d28463e3722a55a1334c4
zidan44 <tuhinsultana075@gmail.com>
2020-03-07 13:57:25 +0600

    Merge tag 'v4.14.112' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable into ten
    This is the 4.14.112 stable release
    
    Signed-off-by: zidan44 <tuhinsultana075@gmail.com>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -151,6 +151,12 @@ void dump_backtrace(struct pt_regs *regs, struct task_struct *tsk)
 
 	pr_debug("%s(regs = %p tsk = %p)\n", __func__, regs, tsk);
 
+	if (regs) {
+		if (user_mode(regs))
+			return;
+		skip = 1;
+	}
+
 	if (!tsk)
 		tsk = current;
 
@@ -171,7 +177,6 @@ void dump_backtrace(struct pt_regs *regs, struct task_struct *tsk)
 	frame.graph = tsk->curr_ret_stack;
 #endif
 
-	skip = !!regs;
 	printk("Call trace:\n");
 	while (1) {
 		unsigned long stack;
@@ -240,15 +245,13 @@ static int __die(const char *str, int err, struct pt_regs *regs)
 		return ret;
 
 	print_modules();
-	__show_regs(regs);
 	pr_emerg("Process %.*s (pid: %d, stack limit = 0x%p)\n",
 		 TASK_COMM_LEN, tsk->comm, task_pid_nr(tsk),
 		 end_of_stack(tsk));
+	show_regs(regs);
 
-	if (!user_mode(regs)) {
-		dump_backtrace(regs, tsk);
+	if (!user_mode(regs))
 		dump_instr(KERN_EMERG, regs);
-	}
 
 	return ret;
 }
